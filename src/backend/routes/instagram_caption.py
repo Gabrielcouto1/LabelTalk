b5d13af903d057638ed8cd6470371fc83ebc91be
Gabriel Couto <gabrielcouto3.14@gmail.com>
2023-09-19 17:29:26 -0300

    feat: insta captions route added, need to add openAI key

---
+++ src/backend/routes/instagram_caption.py
@@ -0,0 +1,35 @@
+import json
+import os
+from utils.rekognition.detect_labels import detect_labels
+from utils.openai.get_completion import get_completion
+
+def handler(event, context):
+    json_body = json.loads(event["body"])
+
+    try:
+        image_name    = json_body['image_name']
+        adjective     = json_body['adjective']
+        endpoint_type = json_body['endpoint_type']
+
+        max_labels    = 10
+        bucket_name   = os.environ['BUCKET_NAME'] 
+    except:
+        return {"statusCode": 500,
+                "headers": {"Content-Type": "application/json"},
+                "body": json.dumps({"errorMsg": "Invalid request."})
+                }
+    try:
+        labels = detect_labels(bucket_name, image_name, max_labels)
+
+        openAI_response = get_completion(endpoint_type, labels, adjective)
+
+        return {"statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(openAI_response)
+           }
+    
+    except:
+        return {"statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"errorMsg": "OpenAi or Rekognition error"})
+           }
