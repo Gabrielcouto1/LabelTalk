fdd032eeef0311b9a7a43c48dccdeb10bad41b5f
Gabriel Couto <gabrielcouto3.14@gmail.com>
2023-09-19 17:26:16 -0300

    feat: created openAI function to get completion

---
+++ src/backend/utils/openai/get_completion.py
@@ -0,0 +1,35 @@
+import requests
+import os
+
+api_key = os.environ['OPEN_AI_KEY']
+
+def get_completion(endpoint_type, labelsList, adjective):
+    url = "https://api.openai.com/v1/engines/davinci/completions"
+
+    prompt = os.environ[endpoint_type]
+
+    for label in labelsList:
+        prompt += label +', '
+    
+    prompt += "Subtitle Adjective: "+ adjective
+
+    # Dados da solicitação
+    data = {
+        "prompt": prompt,
+        "max_tokens": 50,
+    }
+
+    # Passando a chave da API
+    headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
+
+    # Enviando solicitação POST
+    response = requests.post(url, json=data, headers=headers)
+    return {"url": url,
+            "data": data,
+            "headers": headers}
+
+    if response.status_code == 200:
+        result = response.json()
+        return result["choices"][0]["text"]
+    else:
+        return f"Erro: {response.status_code}"
