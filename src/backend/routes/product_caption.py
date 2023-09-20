cfc1471d868f6efb5be775f47004ebe0d89b282b
CarlosLivius <carlinhoslivius123@gmail.com>
2023-09-20 13:20:16 -0300

    feat: product route added successfully

---
+++ src/backend/routes/product_caption.py
@@ -0,0 +1,35 @@
+import json
+import os
+from utils.rekognition.detect_labels import detect_labels
+from utils.openai.get_completion import get_completion
+
+
+def handler(event, context):
+    json_body = json.loads(event["body"])
+
+    try:
+        image_name      = json_body['image_name']
+        adjective       = json_body['adjective']
+
+        max_labels      = 10
+        endpoint_type   = 'PRODUCT_PROMPT'
+        bucket_name     = os.environ['BUCKET_NAME']
+    except:
+        return {
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"errorMsg": "Invalid request"}),
+        }
+    try:
+        labels = detect_labels(bucket_name, image_name, max_labels)
+
+        openAI_response = get_completion(endpoint_type, labels, adjective)
+
+        return openAI_response
+
+    except:
+        return {
+            "statusCode": 500,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps({"errorMsg": "OpeAi or Rekognition error"}),
+        }
