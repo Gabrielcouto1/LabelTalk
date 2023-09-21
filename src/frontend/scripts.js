640ce0e7e0865e2954f8b02aea6317fe7a255a33
Gabriel Couto <gabrielcouto3.14@gmail.com>
2023-09-21 12:24:10 -0300

    feat: initial tests on frontend requests with new finch plugin

---
+++ src/frontend/scripts.js
@@ -0,0 +1,22 @@
+function testePost(){
+    var form = {
+        "image_name": "produto_geladeira2.jpg",
+        "adjective": "grande"
+    }
+
+    console.log(form);
+
+    fetch("https://24hj51kpaf.execute-api.us-east-1.amazonaws.com/product", {
+        method: "POST",
+        headers: {'content-type': 'application/json',
+                  'Access-Control-Allow-Origin': '*',
+                 },
+        body: JSON.stringify(form),
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data);
+        document.getElementById('headline').innerText = data.headline
+        document.getElementById('descricao').innerText = data.descricao
+    })
+}
