bec2725af9d069e9aff592d160c11a5f99d2ba3d
Gabriel Couto <gabrielcouto3.14@gmail.com>
2023-09-22 11:49:27 -0300

    fix: fixed redirect location ref on landing page

--- src/backend/routes/landing_page.py
+++ src/backend/routes/landing_page.py
@@ -1,6 +1,6 @@
 def handler(event, context):
     response = {
-        "headers": {"Location": "http://label-talk-frontend.s3-website-us-east-1.amazonaws.com", },
+        "headers": {"Location": "http://label-talk.s3-website-us-east-1.amazonaws.com", },
         "statusCode": 301,
     }
 
